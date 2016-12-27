9406494e152e50c323692696ef94eb65e83be175
vibakar <vibakar23@gmail.com>
2016-12-27 20:46:34 +0530

    'heroku'

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const hbs=require('hbs');
 
+const port=process.env.PORT || 3000;
 var app=express();
 app.set('view engine','hbs');
 hbs.registerPartials(__dirname+"/views/partials");
@@ -29,6 +30,6 @@ app.get("/bad",(req,res)=>{
   });
 });
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
   console.log("server started");
 });
