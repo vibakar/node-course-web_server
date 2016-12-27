b9cc7d9b5f37bda97d35c1a6e5e8021625d65293
vibakar <vibakar23@gmail.com>
2016-12-27 19:43:41 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express=require('express');
+const hbs=require('hbs');
+
+var app=express();
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+"/views/partials");
+app.use(express.static(__dirname+"/public"));
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+app.get("/",(req,res)=>{
+  res.render("home.hbs",{
+    pageTitle:'Home Page',
+    welcome: 'Welcome to my Website',
+    currentYear:new Date().getFullYear()
+  });
+});
+
+app.get("/about",(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:'About Page',
+    currentYear:new Date().getFullYear()
+  });
+});
+
+app.get("/bad",(req,res)=>{
+  res.send({
+    errorMessage:"Unable to handle the request"
+  });
+});
+
+app.listen(3000,()=>{
+  console.log("server started");
+});
